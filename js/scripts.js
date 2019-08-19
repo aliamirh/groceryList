ccf3e6583b20783e26ef869a39c164adc13e485c
Student <example@example.com>
2019-08-19 15:25:53 -0700

    add forms with lists and loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,41 @@
 $(document).ready(function(){
   $("form#list").submit(function(event){
     event.preventDefault();
+    $("form#list").hide();
+    var groceryItems = [];
+    //
+    // groceryItems[0] = $("input#item1").val();
+    // groceryItems[1] = $("input#item2").val();
+    // groceryItems[2] = $("input#item3").val();
+
+    var index = 0;
+
+
+    for ( i = 0; i < 3; i++){
+      groceryItems[i] = $("input#item" + (i+1) ).val();
+    }
 
 
 
+
+    var capitalizedItems = groceryItems.map(function(item){
+      return item.toUpperCase();
+      console.log(item);
+    });
+
+    capitalizedItems.sort();
+
+    console.log(capitalizedItems);
+    var i =0;
+
+
+    capitalizedItems.forEach(function(item, i){
+      i +=1;
+      $("li#listItem" + i).append(item);
+
+    });
+
+    //insert output into li item ids
 
 
 
